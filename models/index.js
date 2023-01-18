16fbaa4a1f7dc8f75e4616e5ff7f048dc95a2742
“Gigi <debortolg@gmail.com>
2023-01-18 00:53:11 -0800

    trying to get page to render - looking stack overflow for errors

--- models/index.js
+++ models/index.js
@@ -1,34 +1,34 @@
-// import all models
-const Post = require('./Post');
 const User = require('./User');
+const Post = require('./Post');
 const Comment = require('./Comment');
+
+User.hasMany(Post,{
+    foreignKey: 'user_id'
+});git
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    // onDelete: 'SET NULL'
+});
 
 Comment.belongsTo(User, {
     foreignKey: 'user_id',
-  //   onDelete: 'SET NULL'
-  });
-  
+    onDelete: 'CASCADE'
+});
+
 Comment.belongsTo(Post, {
     foreignKey: 'post_id',
-    onDelete: 'SET NULL'
-});
-  
-User.hasMany(Post, {
-    git foreignKey: 'user_id'
+    onDelete: 'CASCADE'
 });
 
 User.hasMany(Comment, {
     foreignKey: 'user_id',
     // onDelete: 'SET NULL'
-  });
+});
 
-Post.belongsTo(User, {
-    foreignKey: 'user_id',
-    // onDelete: 'SET NULL'
-  });
-  
 Post.hasMany(Comment, {
     foreignKey: 'post_id'
 });
 
 module.exports = { User, Post, Comment };
+
