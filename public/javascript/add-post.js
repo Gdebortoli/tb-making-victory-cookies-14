ad4b75806d0a41bf58ad912f598a33128cd18b54
“Gigi <debortolg@gmail.com>
2023-01-18 23:51:38 -0800

    got the page functioning with the help of a kind BCS assistant

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -2,13 +2,13 @@ async function newFormHandler(event) {
   event.preventDefault();
 
   const title = document.querySelector('input[name="post-title"]').value;
-  const post_content = document.querySelector('input[name="post_content"]').value;
+  const post_content = document.querySelector('input[name="post-content"]');
 
   const response = await fetch(`/api/posts`, {
     method: 'POST',
     body: JSON.stringify({
       title,
-      post_content,
+      post_content
     }),
     headers: {
       'Content-Type': 'application/json'
