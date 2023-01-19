f82b2faa965947bd567092174e348b003fc6d9a1
“Gigi <debortolg@gmail.com>
2023-01-19 00:36:03 -0800

    added edit post functionality

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,17 +1,14 @@
 module.exports = {
+  // Helper from hbs that formats the date 
     format_date: date => {
       return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
         date
       ).getFullYear()}`;
     },
-    // format_url: url => {
-    //   return url
-    //     .replace('http://', '')
-    //     .replace('https://', '')
-    //     .replace('www.', '')
-    //     .split('/')[0]
-    //     .split('?')[0];
+    // format_content: content => {
+    //
     // },
+    // Helper for plurals
     format_plural: (word, amount) => {
       if (amount !== 1) {
         return `${word}s`;
